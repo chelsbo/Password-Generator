e3091b70fd0fb9fd83bfd9b7c811f927897dc26f
chelsbo <chelsea.reese@icloud.com>
2022-08-04 21:39:55 -0600

    Sync Files

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var characters = "abcdefghijklmnopqrstuvwxyz123456789!?*&$%#"
+var password = ""
+// Write password to the #password input
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * characters.length);
+    password = chars.substring(randomNumber, randomNumber +1);
+   }
+  passwordText.value = password;
+  document.getElementById("password").value = password;
+}
+// iejkf;lksadjf
+function copyPassword() {
+ var copyText = document.getElementById("password");
+ copyText.select();  
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
